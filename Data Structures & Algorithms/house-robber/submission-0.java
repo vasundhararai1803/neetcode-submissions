31a3b028fe79971637aa5b0998aacac89de2939b
vasundhararai1803 <vasundhara.rai2003@gmail.com>
2026-09-14 03:30:23 +0530

    Add: house-robber - submission-0

---
+++ Data Structures & Algorithms/house-robber/submission-0.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+        dp[1] = nums[0];
+
+        for(int i=2; i<=n; i++){
+            int skip = dp[i-1];
+            int take = nums[i-1] + dp[i-2];
+
+            dp[i] = Math.max(skip, take);
+        }
+        return dp[n];
+    }
+}
